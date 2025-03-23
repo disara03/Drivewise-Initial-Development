4e837cc167b20375d7571d72da9e2fbe70c466ae
Chaniru Mannapperuma <chanirumannapperuma18@gmail.com>
2025-03-23 17:02:29 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,8 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
 
+        //tdryfgvhbtrdyfgyjhb yftgh
+
 
         /fcghbjknml;
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
