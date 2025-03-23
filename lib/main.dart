719edfdb68d000228f5bc00e133830df937e5808
M . H . Disara Sewmini <163980937+disara03@users.noreply.github.com>
2025-03-23 16:45:33 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
+
+
+        /fcghbjknml;
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
